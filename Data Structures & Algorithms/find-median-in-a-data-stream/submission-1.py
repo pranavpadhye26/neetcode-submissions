d51b54bb1caef79aa1c94fc82c614d7409d235a4
Pranav Padhye <133632754+pranavpadhye26@users.noreply.github.com>
2026-04-30 19:28:06 -0700

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,35 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = [] 
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -num)
+
+        if self.large and -self.small[0] > self.large[0]:
+            value = -heapq.heappop(self.small)
+            heapq.heappush(self.large,value)
+        if len(self.small) > 1 + len(self.large):
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+        if len(self.large) > len(self.small):
+            heapq.heappush(self.small, - heapq.heappop(self.large))
+        # heapq.heappush(self.small,-num)
+        # if self.large and -self.small[0] > self.large[0]:
+        #     value = -heapq.heappop(self.small)
+        #     heapq.heappush(self.large,value)
+        # if len(self.small) > len(self.large) + 1:
+        #     heapq.heappush(self.large, -heapq.heappop(self.small))
+        # if len(self.large) > len(self.small):
+        #     heapq.heappush(self.small, -heapq.heappop(self.large))
+
+    def findMedian(self) -> float:
+        # if len(self.small) == len(self.large):
+        #     return (-self.small[0] + self.large[0]) / 2
+        # else:
+        #     return -self.small[0]
+        if len(self.small) == len(self.large):
+            return (-self.small[0] + self.large[0]) / 2
+        else:
+            return -self.small[0]
+        
